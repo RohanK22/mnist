df513e98e9c293374385a29682de6427b02c3ec6
RohanK22 <rohank2205@gmail.com>
2021-01-12 17:19:39 +0000

    Minor Refactors

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ trainingExamples = pd.read_csv ('./data/mnist_train.csv')
 print('Loaded training examples')
 
 # 1D Array of all the labels to the img data 
-labels = trainingExamples.iloc[:, 0].to_numpy()
+ys = trainingExamples.iloc[:, 0].to_numpy()
 
 # 2D Array of all the images (28 x 28) with their grayscale values
-images = trainingExamples.iloc[:, 1:].to_numpy()
+trainingSet = trainingExamples.iloc[:, 1:].to_numpy()
 
 
 class Network:
@@ -61,4 +61,4 @@ layer = Layer(5, None)
 layer2 = Layer(3, layer)
 layer2.show()
 """
-print(len(images[0]))      
+print(len(trainingSet[0]))      
